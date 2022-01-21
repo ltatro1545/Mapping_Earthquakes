27f67a6e30409938534691bbac4367ad4939eef0
ltatro1545 <92493572+ltatro1545@users.noreply.github.com>
2022-01-21 18:07:05 -0500

    added logic file to add marker to map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -10,10 +10,22 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // });
 
 // Create tile layer that will be the background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // Then add our graymap tile layer to the map
 streets.addTo(map);
+
+// ################### MODULE 13.4.1 MAP A SINGLE POINT
+var marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// add a circle to the map (radius is in meters!)
+L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: "#ffffa1"
+}).addTo(map);
+// Can do exact same function a s above, but with L.circleMarker to have the radius measured in pixels!
+
