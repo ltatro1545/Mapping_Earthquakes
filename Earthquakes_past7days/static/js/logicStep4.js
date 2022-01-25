62212233201fe5e938ec3de756302edb850f6555
ltatro1545 <92493572+ltatro1545@users.noreply.github.com>
2022-01-25 15:43:48 -0500

    added overlay to remove or add earthquakes on map

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -101,5 +101,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
     }
   }).addTo(earthquakes);
+  earthquakes.addTo(map);
 });
 
